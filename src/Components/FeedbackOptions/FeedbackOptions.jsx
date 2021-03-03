0b47a22fa67710b4d03a9de7d0e855559c2f2ccf
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-03-03 22:03:10 +0200

    Виконав ДЗ 2(1)

---
+++ src/Components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          className={styles[option]}
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+          key={option}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+};
+
+export default FeedbackOptions;
---
+++ src/Components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <div>
+      <p className={styles.good}>
+        Good: <span>{good}</span>
+      </p>
+      <p className={styles.neutral}>
+        Neutral: <span>{neutral}</span>
+      </p>
+      <p className={styles.bad}>
+        Bad: <span>{bad}</span>
+      </p>
+      <p>
+        Total:
+        <span> {total}</span>
+      </p>
+      <p className={styles.total}>
+        Positive feedback:
+        {positivePercentage}
+      </p>
+    </div>
+  );
+};
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+};
+export default Statistics;
